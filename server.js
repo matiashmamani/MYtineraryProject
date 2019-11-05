8e9800bdfb50f83c8a808d2d007980be51c813e2
alecada6 <alecada6@gmail.com>
2019-11-04 21:49:49 -0300

    backend

---
+++ server.js
@@ -0,0 +1,39 @@
+// server.js
+
+// BASE SETUP
+// ==============================================
+
+var express = require('express');
+var app     = express();
+var port    =   process.env.PORT || 5000;
+
+// ROUTES
+// ==============================================
+
+// sample route with a route the way we're used to seeing it
+app.get('/sample', function(req, res) {
+    res.send('this is a sample!');  
+});
+
+// we'll create our routes here
+
+// get an instance of router
+var router = express.Router();
+
+// home page route (http://localhost:8080)
+router.get('/', function(req, res) {
+    res.send('im the home page!');  
+});
+
+// about page route (http://localhost:8080/about)
+router.get('/test', function(req, res) {
+    res.send('HELLO WORLD'); 
+});
+
+// apply the routes to our application
+app.use('/', router);
+
+// START THE SERVER
+// ==============================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
