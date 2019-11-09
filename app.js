744865cdf4901778247c57e2b04721b8061ddc03
alecada6 <alecada6@gmail.com>
2019-11-08 21:55:43 -0300

    'render'

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express    = require('express');
 const bodyParser = require('body-parser');
 const cityRouter = require('./route/cityRouter');
+const cors       = require('cors');
 const app        = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use('/city', cityRouter);
+app.use('/city', cors(), cityRouter);
 
 module.exports = app;
